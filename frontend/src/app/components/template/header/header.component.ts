05df718d1a5c9ea0aa59b240153fca0611c4b1cc
= <=>
2020-10-29 18:44:34 -0300

    Atualizando titulos no Header e conclusão do projeto

--- frontend/src/app/components/template/header/header.component.ts
+++ frontend/src/app/components/template/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HeaderService } from "./header.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private headerService: HeaderService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  get title(): string {
+    return this.headerService.headerData.title;
   }
 
+  get icon(): string {
+    return this.headerService.headerData.icon;
+  }
+  get routeUrl(): string {
+    return this.headerService.headerData.routeUrl;
+  }
 }
--- frontend/src/app/views/product/crud/crud.component.ts
+++ frontend/src/app/views/product/crud/crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { HeaderService } from "src/app/components/template/header/header.service";
 
 @Component({
   selector: "app-crud",
@@ -7,7 +8,13 @@ import { Router } from "@angular/router";
   styleUrls: ["./crud.component.css"],
 })
 export class CrudComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: "Cadastro de Produtos",
+      icon: "storefront",
+      routeUrl: "/products",
+    };
+  }
 
   ngOnInit(): void {}
 
